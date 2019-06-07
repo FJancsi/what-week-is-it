b980203e25005c0314463405028baaa4440fc46d
Janos Fekete <v-jfekete@hotels.com>
2019-06-07 16:09:11 +0200

    Upgraded to babel 7 + fixed low security vulnerabilities

--- src/index.test.js
+++ src/index.test.js
@@ -2,5 +2,5 @@
 import { whatWeekIsIt } from 'what-week-is-it';
 
 test('what week is it', () => {
-    expect(whatWeekIsIt()).toBe(22); //should be up-to-date :)
+    expect(whatWeekIsIt()).toBe(23); //should be up-to-date :)
 });
