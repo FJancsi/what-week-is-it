1654593e82e4308916c684b595abaada8bdc9b58
FJancsi <fekete.janos2@gmail.com>
2019-08-27 10:49:30 +0200

    npm audit fix

--- src/index.test.js
+++ src/index.test.js
@@ -2,5 +2,5 @@
 import { whatWeekIsIt } from 'what-week-is-it';
 
 test('what week is it', () => {
-    expect(whatWeekIsIt()).toBe(29); //should be up-to-date :)
+    expect(whatWeekIsIt()).toBe(35); //should be up-to-date :)
 });
