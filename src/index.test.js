dd24211dcc62b2d463fb329bf3bb07bbfc817b82
FJancsi <fekete.janos2@gmail.com>
2019-10-10 10:08:35 +0200

    npm audit fix, lockfile-linter, unit-test fix

--- src/index.test.js
+++ src/index.test.js
@@ -2,5 +2,5 @@
 import { whatWeekIsIt } from 'what-week-is-it';
 
 test('what week is it', () => {
-    expect(whatWeekIsIt()).toBe(35); //should be up-to-date :)
+    expect(whatWeekIsIt()).toBe(41); //should be up-to-date :)
 });
