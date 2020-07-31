fab5d0d32cbf48343dcbc21bf802fc9b2315943a
FJancsi <fekete.janos2@gmail.com>
2020-07-31 12:39:30 +0200

    security and test fixes

--- src/index.test.js
+++ src/index.test.js
@@ -2,5 +2,5 @@
 import { whatWeekIsIt } from 'what-week-is-it';
 
 test('what week is it', () => {
-    expect(whatWeekIsIt()).toBe(13); //should be up-to-date :)
+    expect(whatWeekIsIt()).toBe(31); //should be up-to-date :)
 });
